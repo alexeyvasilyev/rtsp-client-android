f53cf8679252383ed95ca7097958e6f971a7285d
Fruchtzwerg94 <Fruchtzwerg94@users.noreply.github.com>
2022-01-09 16:16:12 +0100

    Catch potential MediaCodec.CodecException for getInputBuffer

--- library-client-rtsp/src/main/java/com/alexvas/rtsp/codec/AudioDecodeThread.kt
+++ library-client-rtsp/src/main/java/com/alexvas/rtsp/codec/AudioDecodeThread.kt
@@ -60,7 +60,13 @@ class AudioDecodeThread (
             val inIndex: Int = decoder.dequeueInputBuffer(10000L)
             if (inIndex >= 0) {
                 // fill inputBuffers[inputBufferIndex] with valid data
-                val byteBuffer: ByteBuffer? = decoder.getInputBuffer(inIndex)
+                var byteBuffer: ByteBuffer?
+                try {
+                    byteBuffer = decoder.getInputBuffer(inIndex)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    break
+                }
                 byteBuffer?.rewind()
 
                 // Preventing BufferOverflowException
