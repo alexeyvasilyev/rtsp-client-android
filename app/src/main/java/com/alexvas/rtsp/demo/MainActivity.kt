2d4380dc1592f36d35d64e86777ad50d88949ce7
John Petrocik <john@petrocik.net>
2022-10-06 00:23:26 -0700

    Fullscreen

--- app/src/main/java/com/alexvas/rtsp/demo/MainActivity.kt
+++ app/src/main/java/com/alexvas/rtsp/demo/MainActivity.kt
@@ -11,14 +11,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+//        val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 //        val appBarConfiguration = AppBarConfiguration(setOf(
 //                R.id.navigation_live, R.id.navigation_logs))
 //        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+//        navView.setupWithNavController(navController)
     }
 }
