325295720cc5bf263379979a1c6fb8ba8ad28700
John Petrocik <john@petrocik.net>
2022-10-06 00:23:26 -0700

    WIP Fullscreen

--- app/src/main/java/com/alexvas/rtsp/demo/live/LiveFragment.kt
+++ app/src/main/java/com/alexvas/rtsp/demo/live/LiveFragment.kt
@@ -29,12 +29,12 @@ class LiveFragment : Fragment() {
             binding.tvStatus.text = "RTSP connecting"
             binding.pbLoading.visibility = View.VISIBLE
             binding.vShutter.visibility = View.VISIBLE
-            binding.etRtspRequest.isEnabled = false
-            binding.etRtspUsername.isEnabled = false
-            binding.etRtspPassword.isEnabled = false
-            binding.cbVideo.isEnabled = false
-            binding.cbAudio.isEnabled = false
-            binding.cbDebug.isEnabled = false
+//            binding.etRtspRequest.isEnabled = false
+//            binding.etRtspUsername.isEnabled = false
+//            binding.etRtspPassword.isEnabled = false
+//            binding.cbVideo.isEnabled = false
+//            binding.cbAudio.isEnabled = false
+//            binding.cbDebug.isEnabled = false
         }
 
         override fun onRtspStatusConnected() {
@@ -48,13 +48,13 @@ class LiveFragment : Fragment() {
             binding.bnStartStop.text = "Start RTSP"
             binding.pbLoading.visibility = View.GONE
             binding.vShutter.visibility = View.VISIBLE
-            binding.bnSnapshot.isEnabled = false
-            binding.cbVideo.isEnabled = true
-            binding.cbAudio.isEnabled = true
-            binding.cbDebug.isEnabled = true
-            binding.etRtspRequest.isEnabled = true
-            binding.etRtspUsername.isEnabled = true
-            binding.etRtspPassword.isEnabled = true
+//            binding.bnSnapshot.isEnabled = false
+//            binding.cbVideo.isEnabled = true
+//            binding.cbAudio.isEnabled = true
+//            binding.cbDebug.isEnabled = true
+//            binding.etRtspRequest.isEnabled = true
+//            binding.etRtspUsername.isEnabled = true
+//            binding.etRtspPassword.isEnabled = true
         }
 
         override fun onRtspStatusFailedUnauthorized() {
@@ -73,7 +73,7 @@ class LiveFragment : Fragment() {
 
         override fun onRtspFirstFrameRendered() {
             binding.vShutter.visibility = View.GONE
-            binding.bnSnapshot.isEnabled = true
+//            binding.bnSnapshot.isEnabled = true
         }
     }
 
@@ -106,76 +106,76 @@ class LiveFragment : Fragment() {
         binding = FragmentLiveBinding.inflate(inflater, container, false)
 
         binding.svVideo.setStatusListener(rtspStatusListener)
-        binding.etRtspRequest.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val text = s.toString()
-                if (text != liveViewModel.rtspRequest.value) {
-                    liveViewModel.rtspRequest.value = text
-                }
-            }
-        })
-        binding.etRtspUsername.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val text = s.toString()
-                if (text != liveViewModel.rtspUsername.value) {
-                    liveViewModel.rtspUsername.value = text
-                }
-            }
-        })
-        binding.etRtspPassword.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val text = s.toString()
-                if (text != liveViewModel.rtspPassword.value) {
-                    liveViewModel.rtspPassword.value = text
-                }
-            }
-        })
-
-        liveViewModel.rtspRequest.observe(viewLifecycleOwner, {
-            if (binding.etRtspRequest.text.toString() != it)
-                binding.etRtspRequest.setText(it)
-        })
-        liveViewModel.rtspUsername.observe(viewLifecycleOwner, {
-            if (binding.etRtspUsername.text.toString() != it)
-                binding.etRtspUsername.setText(it)
-        })
-        liveViewModel.rtspPassword.observe(viewLifecycleOwner, {
-            if (binding.etRtspPassword.text.toString() != it)
-                binding.etRtspPassword.setText(it)
-        })
-
+//        binding.etRtspRequest.addTextChangedListener(object : TextWatcher {
+//            override fun afterTextChanged(s: Editable?) {
+//            }
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            }
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                val text = s.toString()
+//                if (text != liveViewModel.rtspRequest.value) {
+//                    liveViewModel.rtspRequest.value = text
+//                }
+//            }
+//        })
+//        binding.etRtspUsername.addTextChangedListener(object : TextWatcher {
+//            override fun afterTextChanged(s: Editable?) {
+//            }
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            }
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                val text = s.toString()
+//                if (text != liveViewModel.rtspUsername.value) {
+//                    liveViewModel.rtspUsername.value = text
+//                }
+//            }
+//        })
+//        binding.etRtspPassword.addTextChangedListener(object : TextWatcher {
+//            override fun afterTextChanged(s: Editable?) {
+//            }
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            }
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                val text = s.toString()
+//                if (text != liveViewModel.rtspPassword.value) {
+//                    liveViewModel.rtspPassword.value = text
+//                }
+//            }
+//        })
+//
+//        liveViewModel.rtspRequest.observe(viewLifecycleOwner, {
+//            if (binding.etRtspRequest.text.toString() != it)
+//                binding.etRtspRequest.setText(it)
+//        })
+//        liveViewModel.rtspUsername.observe(viewLifecycleOwner, {
+//            if (binding.etRtspUsername.text.toString() != it)
+//                binding.etRtspUsername.setText(it)
+//        })
+//        liveViewModel.rtspPassword.observe(viewLifecycleOwner, {
+//            if (binding.etRtspPassword.text.toString() != it)
+//                binding.etRtspPassword.setText(it)
+//        })
+//
         binding.bnStartStop.setOnClickListener {
             if (binding.svVideo.isStarted()) {
                 binding.svVideo.stop()
             } else {
-                val uri = Uri.parse(liveViewModel.rtspRequest.value)
-                binding.svVideo.init(uri, liveViewModel.rtspUsername.value, liveViewModel.rtspPassword.value, "rtsp-client-android")
-                binding.svVideo.debug = binding.cbDebug.isChecked
-                binding.svVideo.start(binding.cbVideo.isChecked, binding.cbAudio.isChecked)
+                val uri = Uri.parse("rtsp://192.168.1.201:554/11")
+                binding.svVideo.init(uri, "admin", "102973", "rtsp-client-android")
+                binding.svVideo.debug = false //binding.cbDebug.isChecked
+                binding.svVideo.start(true, true)
             }
         }
-
-        binding.bnSnapshot.setOnClickListener {
-            val bitmap = getSnapshot()
-            // TODO Save snapshot to DCIM folder
-            if (bitmap != null) {
-                Toast.makeText(requireContext(), "Snapshot succeeded", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(requireContext(), "Snapshot failed", Toast.LENGTH_LONG).show()
-            }
-        }
+//
+//        binding.bnSnapshot.setOnClickListener {
+//            val bitmap = getSnapshot()
+//            // TODO Save snapshot to DCIM folder
+//            if (bitmap != null) {
+//                Toast.makeText(requireContext(), "Snapshot succeeded", Toast.LENGTH_LONG).show()
+//            } else {
+//                Toast.makeText(requireContext(), "Snapshot failed", Toast.LENGTH_LONG).show()
+//            }
+//        }
         return binding.root
     }
 
