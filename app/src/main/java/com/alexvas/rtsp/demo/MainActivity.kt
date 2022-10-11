986dfc6021e92d4f1ca2614bb1a730fc1c3cdad1
John Petrocik <john@petrocik.net>
2022-10-10 21:52:16 -0700

    ONVIF

--- app/src/main/java/com/alexvas/rtsp/demo/MainActivity.kt
+++ app/src/main/java/com/alexvas/rtsp/demo/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.alexvas.rtsp.demo
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
+import net.petrocik.onvif.wcf.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sentence = object : UIIIServiceEvents {
+            override fun Starting() {};
+            override fun Completed(result: UIIOperationResult<*>?) {
+                val res: UIIGetDeviceInformationResponse = result!!.Result as ObjectWithListForRPC
+            };
+        }
+
+        var service = UIIDeviceBinding(sentence, "");
+        service.GetDeviceInformationAsync();
+
 //        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
 //        val navController = findNavController(R.id.nav_host_fragment)
